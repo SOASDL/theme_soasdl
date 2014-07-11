62db460d7dab3f095a4391dd384db203aa53347f
bashet <a_bashet@yahoo.com>
2014-07-11 16:30:38 +0100

    add link to soasmail

--- layout/columns2.php
+++ layout/columns2.php
@@ -52,7 +52,7 @@ echo $OUTPUT->doctype() ?>
                     <li class="divider-vertical"></li>
                     <li><a href="<?php echo $CFG->wwwroot;?>/local/faq/view.php">FAQ</a></li>
                     <li class="divider-vertical"></li>
-                    <li><a href="#">Another external link</a></li>
+                    <li><a href="<?php echo $CFG->wwwroot;?>/blocks/soasmail/mailbox.php">Mail</a></li>
                     <li class="divider-vertical"></li>
                 </ul>
 
