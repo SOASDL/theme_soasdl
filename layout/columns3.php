6e1fa03a94bd787b7c60d569de7ad82b3c1cb47f
bashet <bashet01>
2015-03-06 16:38:53 +0000

    use the session data to determine if it need to show again!

--- layout/columns3.php
+++ layout/columns3.php
@@ -134,21 +134,33 @@ echo $OUTPUT->doctype() ?>
         ?>
     </footer>
 
-    <div id="myModal" class="modal hide fade">
-        <div class="modal-header">
-            <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-            <h3>Welcome to your e-learning Centre</h3>
+
+    <?php
+    if (!$_SESSION['GoToFAQ']) {
+
+        $_SESSION['GoToFAQ']=true;
+
+        ?>
+        <div id="myModal" class="modal hide fade">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                <h3>Welcome to your e-learning Centre</h3>
+            </div>
+            <div class="modal-body">
+                <p>If this is your first visit, please go to the FAQ section where you can explore the features available here.
+                </p>
+            </div>
+            <div class="modal-footer">
+                <label style="float: left"><input type="checkbox">Do not show this again!</label>
+                <button class="btn" data-dismiss="modal" aria-hidden="true">Close</button>
+                <a href="<?php echo $CFG->wwwroot?>/local/faq/view.php" class="btn btn-primary">Go to FAQ</a>
+            </div>
         </div>
-        <div class="modal-body">
-            <p>If this is your first visit, please go to the FAQ section where you can explore the features available here.
-            </p>
-        </div>
-        <div class="modal-footer">
-            <label style="float: left"><input type="checkbox">Do not show this again!</label>
-            <button class="btn" data-dismiss="modal" aria-hidden="true">Close</button>
-            <a href="<?php echo $CFG->wwwroot?>/local/faq/view.php" class="btn btn-primary">Go to FAQ</a>
-        </div>
-    </div>
+    <?php
+
+    }
+
+    ?>
 
     <?php echo $OUTPUT->standard_end_of_body_html() ?>
 
